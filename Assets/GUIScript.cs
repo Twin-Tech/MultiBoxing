7460e49d8636c374dac9503440b4906888ff8daf
Hemang Shah <hemangshah97@gmail.com>
2018-03-19 22:49:52 +0530

    Gui done

--- Assets/GUIScript.cs
+++ Assets/GUIScript.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using UnityEditor;
 using UnityEngine.UI;
 
+
 public class GUIScript : MonoBehaviour {
 
+    private Dictionary<string, int> selectedExercise;
+    private List<string> selectedJoints;
+    string currExercise;
+    private int breakTime, sets;
     private string user;
+    private GameObject loginUI, inter, skel;
 	// Use this for initialization
 	void Start () {
-
+        loginUI = GameObject.Find("Login");
+        inter = GameObject.Find("Interface");
+        skel = GameObject.Find("Skeleton");
+        inter.SetActive(false);
+        skel.SetActive(false);
+        //currExercise = new Exercise();
+        selectedExercise = new Dictionary<string, int>();
+        selectedJoints = new List<string>();
 	}
 	
 	// Update is called once per frame
@@ -73,6 +87,86 @@ public class GUIScript : MonoBehaviour {
     }
     public void loginSuccess()
     {
-        GameObject.Find("Login").active = false;
+        loginUI.SetActive(false);
+        inter.SetActive(true);
+        skel.SetActive(true);
+    }
+
+
+    public void OKdemo(InputField ip)
+    {
+        if (ip.text != "")
+        {
+            int rep = System.Convert.ToInt32(ip.text);
+            selectedExercise[currExercise] = rep;
+
+            foreach (KeyValuePair<string, int>  k in selectedExercise)
+            {
+                Debug.Log(k.Key+":"+k.Value);
+            }
+            currExercise = null;
+
+            //Debug.Log(currExercise.name + ":" + currExercise.reps);
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Error", "Enter the number of Reps.", "Okay");
+        }
+    }
+
+    public void ExercisePressed(Button b)
+    {
+        if (b.image.color == Color.green)
+        {
+            EditorUtility.DisplayDialog("Error", "You have already selected this exercise.", "Okay");
+            currExercise = b.name;
+        }
+        else
+        {
+            b.image.color = Color.green;
+            currExercise = b.name;
+            selectedExercise.Add(currExercise, 0);
+        }
+    }
+
+    public void addJoint(Button b)
+    {
+        string jointName = b.name;
+        if (selectedJoints.Contains(jointName))
+        {
+            selectedJoints.Remove(jointName);
+            b.image.color = Color.white;
+        }
+        else
+        {
+            selectedJoints.Add(jointName);
+            b.image.color = Color.green;
+        }
+        foreach (string s in selectedJoints)
+        {
+            Debug.Log(s);
+        }
+    }
+
+    public void finish()
+    {
+        InputField br = GameObject.Find("BreakField").GetComponent<InputField>();
+        InputField set = GameObject.Find("SetsField").GetComponent<InputField>();
+        if(selectedJoints.Count==0)
+            EditorUtility.DisplayDialog("Error", "You have not selected any joints.", "Okay");
+        else if(selectedExercise.Count==0)
+            EditorUtility.DisplayDialog("Error", "You have not selected any exercise.", "Okay");
+        else if(br.text=="")
+            EditorUtility.DisplayDialog("Error", "You have not entered break time.", "Okay");
+        else if (set.text == "")
+            EditorUtility.DisplayDialog("Error", "You have not entered number of sets.", "Okay");
+        else
+        {
+            breakTime = Convert.ToInt32(br.text);
+            sets = Convert.ToInt32(set.text);
+            inter.SetActive(false);
+            skel.SetActive(false);
+            EditorUtility.DisplayDialog("Error", "Details Updated.", "Okay");
+        }
     }
 }
